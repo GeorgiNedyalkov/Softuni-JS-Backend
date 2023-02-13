f758228a7d7ad03c2fb20d3a518c221e9256a2ea
Flu3nt <gnedyalkov94@gmail.com>
2023-02-13 11:11:07 +0200

    Show edit cube page

--- cubicle/src/controllers/cubeController.js
+++ cubicle/src/controllers/cubeController.js
@@ -1,19 +1,17 @@
 const Cube = require("../models/Cube");
 const Accessory = require("../models/Accessory");
+const { getOne } = require("../services/cubeService");
+const cubeUtils = require("../utils/cubeUtils");
 
 exports.getCreateCube = (req, res) => {
-  console.log("req.user");
-  console.log(req.user);
   res.render("create");
 };
 
 exports.postCreateCube = async (req, res) => {
   const { name, description, imageUrl, difficultyLevel } = req.body;
-
   let cube = new Cube({ name, description, imageUrl, difficultyLevel });
 
   await cube.save();
-
   res.redirect("/");
 };
 
@@ -30,6 +28,25 @@ exports.getCubeDetails = async (req, res) => {
 exports.getAttachAccessory = async (req, res) => {
   const cube = await Cube.findById(req.params.cubeId).lean();
   const accessories = await Accessory.find().lean();
-
   res.render("cube/attach", { cube, accessories });
+};
+
+exports.getEditCube = async (req, res) => {
+  const cube = await getOne(req.params.cubeId).lean();
+  const difficultyLevels = cubeUtils.generateDifficultyLevels(
+    cube.difficultyLevel
+  );
+
+  console.log(difficultyLevels);
+
+  res.render("cube/edit", { cube, difficultyLevels });
+};
+
+exports.getDeleteCube = async (req, res) => {
+  const cube = await cubeService.getOne(req.params.cubeId).lean();
+  const difficultyLevels = cubeUtils.generateDifficultyLevels(
+    cube.difficultyLevel
+  );
+
+  res.render("cube/delete", { cube, difficultyLevels });
 };
