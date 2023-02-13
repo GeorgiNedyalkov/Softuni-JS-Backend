f020b1ad5e152efe8b126b0724a55fcd926fdbb4
Flu3nt <gnedyalkov94@gmail.com>
2023-02-13 12:07:11 +0200

    Add logout

--- cubicle/src/middlewares/authMiddleware.js
+++ cubicle/src/middlewares/authMiddleware.js
@@ -1,17 +1,22 @@
-const config = require("../config");
 const jwt = require("../lib/jsonwebtoken");
+const config = require("../config");
 
 exports.authentication = async (req, res, next) => {
   const token = req.cookies["auth"];
 
-  if (!token) {
+  if (token) {
     try {
       const decodedToken = await jwt.verify(token, config.SECRET);
+
       req.user = decodedToken;
       req.isAuthenticated = true;
+
+      res.locals.username = decodedToken.username;
+      res.locals.isAuthenticated = true;
     } catch (error) {
       console.error(error);
       res.clearCookie("auth");
+      res.redirect("/404");
     }
   }
 
